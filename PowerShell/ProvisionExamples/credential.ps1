96453a7efe66bc73052a46dca7869f198d42f74c
ScaryDBA <grant@scarydba.com>
2019-04-16 17:41:10 +0000

    Security Fix
    Removed key value

--- PowerShell/ProvisionExamples/credential.ps1
+++ PowerShell/ProvisionExamples/credential.ps1
@@ -2,7 +2,7 @@ import-module sqlps
   
 # create variables  
 $storageAccount = "provisiondiag932"  
-$storageKey = "rqAgLRL2EB4shThdU0P8mFPpe+ab4IjygNYPBs9igWwMVLt9w+5eDIS4VGzmQZAip1UVilXCOHEJVTT9PsupWw=="  
+$storageKey = "xxx"  
 $secureString = convertto-securestring $storageKey  -asplaintext -force  
 $credentialName = "mybackuptoURL"  
   
